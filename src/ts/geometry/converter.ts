e857b19632eacba6bbbc35a4da665b74a194cfe4
Artem Shinkaruk <artasend@gmail.com>
2022-04-02 21:40:01 +0700

    Remove circular dependency in converter

--- src/ts/geometry/converter.ts
+++ src/ts/geometry/converter.ts
@@ -1,6 +1,3 @@
-import { Point } from "./point";
-import { Vector } from "./vector";
-
 export function toRadians(angle: number) {
     return angle * Math.PI / 180;
 }
@@ -9,6 +6,3 @@ export function toDegrees(angle: number) {
     return angle * 180 / Math.PI;
 }
 
-export function pointToVector(point: Point): Vector {
-    return new Vector(point.x, point.y);
-}
